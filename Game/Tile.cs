8b9cc3054147516ba908166da1e4532e2e101904
Dave Lewis <siu06dl@gmail.com>
2024-06-01 10:37:32 +0100

    fix: missed file from commit

--- Game/Tile.cs
+++ Game/Tile.cs
@@ -2,22 +2,56 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Attack.Game
 {
+    internal enum TileType
+    {
+        Piece,
+        Terrain,
+        Border,
+        Obstacle
+    }
+
     internal class Tile
     {
         public Vector2I Position;
 
-        public int Type;
+        public TileType? Type;
 
-        public Tile (Vector2I position, int type)
+        public PieceNode Piece;
+
+        public Vector2 LocalPosition;
+
+        public bool StartingTile;
+
+        public Tile (Vector2I position, Vector2 localPosition, bool startingTile)
         {
             Position = position;
-            Type = type;
+            LocalPosition = localPosition;
+            StartingTile = startingTile;
         }
+
+        public void AddPiece(PieceNode piece)
+        {
+            piece.Position = LocalPosition;
+            Type = TileType.Piece;
+            Piece = piece;
+        }
+
+        public PieceNode GetPiece => Piece;
+
+        public void RemovePiece()
+        {
+            if (Piece == null)
+                return;
+
+            Piece = null;
+            Type = TileType.Terrain;
+        }
+
+        public bool IsEmpty() => Piece == null && Type == TileType.Terrain;
     }
 }
