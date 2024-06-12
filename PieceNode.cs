1d2028515f883433b7ad8fb9ec4731ece83fed0e
Dave Lewis <siu06dl@gmail.com>
2024-06-12 17:43:05 +0100

    feat: update shader for new sprites

--- PieceNode.cs
+++ PieceNode.cs
@@ -36,13 +36,11 @@ public partial class PieceNode : Node2D
     private Team _team;
 
     // consider changing team to class and exposing in godot ui?
-    private static Color red1 = new Color((float)0.6745, (float)0.1961, (float)0.1961, (float)1);
+    private static Color _red1 = new Color(1, 0, 0, 1);
     private static Color red2 = new Color((float)0.3945, (float)0.0997, 0, 1);
     private static Color red3 = new Color((float)0.1086, (float)0.0107, (float)0.0109, 1);
 
-    private static Color blue1 = new Color((float)0.2020, (float)0.1940, (float)0.67, (float)1);
-    private static Color blue2 = new Color((float)0.0060, 0, (float)0.39, 1);
-    private static Color blue3 = new Color((float)0.013, (float)0.011, (float)0.11, 1);
+    private static Color _blue1 = new Color(0, 0, 1, 1);
 
     [Export]
     internal Team Team {
@@ -142,15 +140,11 @@ public partial class PieceNode : Node2D
             //var propertyList = _material.GetPropertyList();
             //var color1 = _material.Get("shader_parameter/color1");
 
-            _material.Set("shader_parameter/color1", red1);
-            _material.Set("shader_parameter/color2", red2);
-            _material.Set("shader_parameter/color3", red3);
+            _material.Set("shader_parameter/color1", _red1);
         }
         else
         {
-            _material.Set("shader_parameter/color1", blue1);
-            _material.Set("shader_parameter/color2", blue2);
-            _material.Set("shader_parameter/color3", blue3);
+            _material.Set("shader_parameter/color1", _blue1);
         }
     }
 
